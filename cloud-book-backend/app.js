d9a79d04a95ba67870e152ce862921808f4a6331
Ahmer2000 <ahmersayyed2000@gmail.com>
2024-04-16 18:14:12 +0530

    ready

--- cloud-book-backend/app.js
+++ cloud-book-backend/app.js
@@ -9,7 +9,7 @@ const app = express()
  
 app.use(cors({
   origin:["https://cloud-book-client.vercel.app"],
-  methods:["POST"],
+  methods:["POST" , "GET" , "PUT" , "DELETE"],
   credentials: true,
 }))//-->middleware
 const port = process.env.PORT;
